6d7a91e5631c94829a50ecc2988de32a79e7b3f2
Gideon <gillxxx@gmail.com>
2021-07-14 19:36:52 +0200

    update sponsors

--- src/components/SponsorsFooter.vue
+++ src/components/SponsorsFooter.vue
@@ -7,19 +7,19 @@
     </v-card-text>
     <v-row justify="space-around">
       <div class="text-center">
-        <a href="https://iron.finance/" target="_blank" :class="{'text-white': darkmode}">
+        <a href="https://farmhero.io?r=start" target="_blank">
           <v-avatar>
             <v-img
               max-height="50"
               max-width="50"
-              src="@/assets/images/iron-finance.gif"
+              src="@/assets/images/farmhero.png"
             ></v-img>
           </v-avatar>
-          <p>Iron Finance</p>
+          <p :class="{'text-white': darkmode}">Farm Hero</p>
         </a>
       </div>
       <div class="text-center">
-        <a href="https://stablegaj.finance/" target="_blank" :class="{'text-white': darkmode}">
+        <a href="https://stablegaj.finance/" target="_blank">
           <v-avatar>
             <v-img
               max-height="50"
@@ -27,19 +27,19 @@
               src="@/assets/images/stablegaj.svg"
             ></v-img>
           </v-avatar>
-          <p>StableGaj</p>
+          <p :class="{'text-white': darkmode}">StableGaj</p>
         </a>
       </div>
       <div class="text-center">
-        <a href="https://www.prismfinance.net/" target="_blank" :class="{'text-white': darkmode}">
+        <a href="http://kogefarm.io/" target="_blank">
           <v-avatar>
             <v-img
               max-height="50"
               max-width="50"
-              src="@/assets/images/prism-finance.jpg"
+              src="@/assets/images/koge.png"
             ></v-img>
           </v-avatar>
-          <p>Prism Finance</p>
+          <p :class="{'text-white': darkmode}">Koge Farm</p>
         </a>
       </div>
     </v-row>
@@ -57,10 +57,6 @@ export default {
     darkmode() {
       return store.userData.darkmode;
     },
-  },
-  created() {
-    mutations.initStore();
-    console.log("Of all existing things some are in our power, and others are not in our power. In our power are thought, impulse, will to get and will to avoid, and, in a word, everything which is our own doing. Things not in our power include the body, property, reputation, office, and, in a word, everything which is not our own doing. Things in our power are by nature free, unhindered, untrammelled; things not in our power are weak, servile, subject to hindrance, dependent on others. Remember then that if you imagine that what is naturally slavish is free, and what is naturally another's is your own, you will be hampered, you will mourn, you will be put to confusion, you will blame gods and men; but if you think that only your own belongs to you, and that what is another's is indeed another's, no one will ever put compulsion or hindrance on you, you will blame none, you will accuse none, you will do nothing against your will, no one will harm you, you will have no enemy, for no harm can touch you.")
   },
 };
 </script>
