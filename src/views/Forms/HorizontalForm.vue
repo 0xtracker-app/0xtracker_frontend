c8b567157f04d3b6d3ff26ec55eac3bf6501c8cf
Kim Francis Corros <corros121@gmail.com>
2021-09-26 23:13:31 +0800

    Re position Go button and add offset to select menu

--- src/views/Forms/HorizontalForm.vue
+++ src/views/Forms/HorizontalForm.vue
@@ -6,52 +6,80 @@
     lazy-validation
   >
     <v-row>
-      <v-col md="11" cols="11">
+      <v-col cols="12">
         <v-row>
           <v-col md="8" cols="12">
-            <label for="" class="font-weight-600 mb-2 d-block text-white">
-              Wallet Address
-            </label>
-            <v-text-field
-              v-model="wallet"
-              :disabled="loading"
-              rounded
-              outlined
-              solo
-              class="font-size-input placeholder-dark input-alternative input-icon"
-              :rules="walletRules"
-              :dark="darkmode"
-              @input="detectWalletType()"
-            >
-              <template slot="prepend-inner">
-                <v-icon size=".875rem">fas fa-wallet</v-icon>
-              </template>
-              <template
-                v-slot:append
-                v-if="!wallet || !connectedWallet || wallet !== connectedWallet"
-              >
-                <span
-                  v-if="wallet || walletType"
-                  class="caption px-2 font-weight-bold text-uppercase"
-                  :class="[walletType ? 'blue--text' : 'red--text']"
+            <v-row>
+              <v-col cols="10">
+                <label for="" class="font-weight-600 mb-2 d-block text-white">
+                  Wallet Address
+                </label>
+                <v-text-field
+                  v-model="wallet"
+                  :disabled="loading"
+                  rounded
+                  outlined
+                  solo
+                  class="font-size-input placeholder-dark input-alternative input-icon"
+                  :rules="walletRules"
+                  :dark="darkmode"
+                  @input="detectWalletType()"
                 >
-                  {{ walletType || "UNSUPPORTED" }}
-                </span>
-                <v-icon @click="setWalletDialog(true)"> fas fa-plug </v-icon>
-              </template>
-              <template v-slot:append v-else-if="connectedWallet">
-                <span
-                  v-if="wallet || walletType"
-                  class="caption px-2 font-weight-bold text-uppercase"
-                  :class="[walletType ? 'blue--text' : 'red--text']"
+                  <template slot="prepend-inner">
+                    <v-icon size=".875rem">fas fa-wallet</v-icon>
+                  </template>
+                  <template
+                    v-slot:append
+                    v-if="
+                      !wallet || !connectedWallet || wallet !== connectedWallet
+                    "
+                  >
+                    <span
+                      v-if="wallet || walletType"
+                      class="caption px-2 font-weight-bold text-uppercase"
+                      :class="[walletType ? 'blue--text' : 'red--text']"
+                    >
+                      {{ walletType || "UNSUPPORTED" }}
+                    </span>
+                    <v-icon @click="setWalletDialog(true)">
+                      fas fa-plug
+                    </v-icon>
+                  </template>
+                  <template v-slot:append v-else-if="connectedWallet">
+                    <span
+                      v-if="wallet || walletType"
+                      class="caption px-2 font-weight-bold text-uppercase"
+                      :class="[walletType ? 'blue--text' : 'red--text']"
+                    >
+                      {{ walletType || "UNSUPPORTED" }}
+                    </span>
+                    <v-icon @click="setWalletDialog(true)" color="green">
+                      fas fa-plug
+                    </v-icon>
+                  </template>
+                </v-text-field>
+              </v-col>
+              <v-col cols="auto" class="text-center">
+                <label for="" class="font-weight-600 mb-3 d-block text-white">
+                  Go!
+                </label>
+                <v-btn
+                  :disabled="loading || !walletType"
+                  elevation="0"
+                  :ripple="false"
+                  height="40"
+                  min-width="40"
+                  width="40"
+                  class="font-weight-600 text-capitalize btn-ls btn-default rounded-circle"
+                  @click="loadPortfolio({ walletAddress: wallet, walletType })"
+                  :dark="darkmode"
                 >
-                  {{ walletType || "UNSUPPORTED" }}
-                </span>
-                <v-icon @click="setWalletDialog(true)" color="green">
-                  fas fa-plug
-                </v-icon>
-              </template>
-            </v-text-field>
+                  <v-icon size="18" color="#5e72e4"
+                    >fa-arrow-circle-right</v-icon
+                  >
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-col>
           <v-col md="4" cols="12">
             <label for="" class="font-weight-600 mb-2 d-block text-white">
@@ -67,7 +95,11 @@
               :items="userProfiles"
               placeholder="Select Profile"
               :dark="darkmode"
-              :menu-props="darkmode ? 'dark' : 'light'"
+              :menu-props="{
+                offsetY: true,
+                dark: darkmode,
+                light: !darkmode,
+              }"
               return-object
               class="font-size-input text-color-dark input-alternative input-focused-alternative input-icon mb-0"
               @input="loadProfile({ profile: $event })"
@@ -82,24 +114,6 @@
             </v-select>
           </v-col>
         </v-row>
-      </v-col>
-      <v-col md="1" sm="1" class="text-center">
-        <label for="" class="font-weight-600 mb-1 d-block text-white">
-          Go!
-        </label>
-        <v-btn
-          :disabled="loading || !walletType"
-          elevation="0"
-          :ripple="false"
-          height="40"
-          min-width="40"
-          width="40"
-          class="font-weight-600 text-capitalize btn-ls btn-default me-2 my-2 rounded-circle"
-          @click="loadPortfolio({ walletAddress: wallet, walletType })"
-          :dark="darkmode"
-        >
-          <v-icon size="18" color="#5e72e4">fa-arrow-circle-right</v-icon>
-        </v-btn>
       </v-col>
     </v-row>
   </v-form>
