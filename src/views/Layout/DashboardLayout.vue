46c07d7a088290606ac02dce6b1f94c1249075dd
Kim Francis Corros <corros121@gmail.com>
2022-02-07 20:45:29 +0800

    Add notification banner for the new partnership

--- src/views/Layout/DashboardLayout.vue
+++ src/views/Layout/DashboardLayout.vue
@@ -13,6 +13,26 @@
         <fade-transition :duration="200" origin="center top" mode="out-in">
           <div style="margin-top: 65px" id="scroll-reference">
             <div
+              style="border-radius: 8px"
+              class="pa-xl-12 pa-md-6 pa-2 mb-4 text-subtitle-2 text-center announcement-banner"
+            >
+              <span class="mr-2">
+                0xTracker.app &#38; Optical.finance announce strategic
+                partnership. More info soon
+              </span>
+              <v-btn
+                class="text-subtitle-2 text-none font-weight-regular black--text"
+                elevation="0"
+                href="https://t.me/bsc0xtracker"
+                small
+                :dark="darkmode"
+                target="_blank"
+                color="white"
+              >
+                Learn more
+              </v-btn>
+            </div>
+            <div
               style="border-radius: 28px; min-height: 90vh"
               :style="{ backgroundColor: darkmode ? '#232228' : '#f3f4fd' }"
               class="pa-xl-12 pa-md-6 pa-2"
@@ -33,6 +53,7 @@
     </v-main>
   </v-app>
 </template>
+
 <script>
 // /* eslint-disable no-new */
 import PerfectScrollbar from "perfect-scrollbar";
@@ -102,6 +123,7 @@ export default {
   },
 };
 </script>
+
 <style lang="scss">
 .darkmodebg {
   background-color: #0c0b10 !important;
@@ -147,5 +169,23 @@ export default {
   -webkit-box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.3);
   box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.3);
   background-color: #5e72e4;
+}
+
+.announcement-banner {
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
